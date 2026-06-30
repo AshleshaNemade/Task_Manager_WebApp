7503c4fb79c3e9971eb39223314c40aa38dbdd7f
AshleshaNemade <ashnemade16@gmail.com>
2026-07-01 01:46:44 +0530

    Initial commit with server running

---
+++ backend/schema.sql
@@ -0,0 +1,32 @@
+-- Database: task_manager
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) DEFAULT 'user'
+);
+
+CREATE TABLE tasks (
+    task_id SERIAL PRIMARY KEY,
+    title VARCHAR(200),
+    description TEXT,
+    status VARCHAR(50) DEFAULT 'Pending',
+    created_by INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE task_assignments (
+    task_id INTEGER REFERENCES tasks(task_id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    PRIMARY KEY(task_id,user_id)
+);
+
+CREATE TABLE activity_logs(
+    log_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    task_id INTEGER REFERENCES tasks(task_id),
+    action VARCHAR(255),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
